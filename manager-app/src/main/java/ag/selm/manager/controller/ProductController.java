aeffcb2563e0ffb64bb589b92855f5fe73235b9e
Yury <yury.s.nazarov@yandex.ru>
2025-07-05 14:11:02 +0300

    Внедряем зависимости. Старайся зависеть от интерфейсов, это позволит в случае необходимости менять реализацию.

--- manager-app/src/main/java/ag/selm/manager/controller/ProductController.java
+++ manager-app/src/main/java/ag/selm/manager/controller/ProductController.java
@@ -1,7 +1,12 @@
 package ag.selm.manager.controller;
 
+import ag.selm.manager.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor // Создает конструктор который содержит аргументы для final свойств
 public class ProductController {
+
+    private final ProductService productService;
 }
--- manager-app/src/main/java/ag/selm/manager/service/DefaultProductService.java
+++ manager-app/src/main/java/ag/selm/manager/service/DefaultProductService.java
@@ -1,7 +1,12 @@
 package ag.selm.manager.service;
 
+import ag.selm.manager.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor // Создает конструктор который содержит аргументы для final свойств
 public class DefaultProductService implements ProductService {
+
+    private final ProductRepository productRepository;
 }
